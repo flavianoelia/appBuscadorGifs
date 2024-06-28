d7007fb3c94f56c7804211c520174da652a4a756
flavianoelia <flavianoelia79@gmail.com>
2024-06-28 20:18:31 -0300

    app buscador de gifs

---
+++ main.js
@@ -0,0 +1,33 @@
+const apiKey = "opAkPaxV7XBl5dRjiiMeB1jySy3PYePz";
+const term = "Schnauzer";
+const llamada = fetch(`
+https://api.giphy.com/v1/gifs/search?
+api_key=${apiKey}
+&q=${term}
+&limit=10
+&offset=0
+&rating=g
+&lang=es
+&bundle=messaging_non_clips
+`);
+
+llamada
+    .then((response) =>{
+        return response.json();
+})
+   .then((results) => {
+        //let url = results.data [0].images.original.url;
+        //console.log(url);
+        results.data.forEach (element =>{
+            console.log (element.images.original.url);
+            let imagen = document.createElement ("img");
+            imagen.setAttribute ("src", element.images.original.url);
+            document.body.appendChild(imagen);
+
+        });
+   })
+   .catch((error) => {
+        console.error(error.message);
+    });
+
+        
