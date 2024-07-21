fee1df956944c97c8ea365828b9a8c97faf2a7ac
flavianoelia <flavianoelia79@gmail.com>
2024-07-20 22:02:13 -0300

    Tomar nuevas búsquedas y limpiar gifs anteriores

--- js/main.js
+++ js/main.js
@@ -1,33 +1,30 @@
 const apiKey = "opAkPaxV7XBl5dRjiiMeB1jySy3PYePz";
-const term = "Schnauzer";
-const llamada = fetch(`
-https://api.giphy.com/v1/gifs/search?
-api_key=${apiKey}
-&q=${term}
-&limit=9
-&offset=0
-&rating=g
-&lang=es
-&bundle=messaging_non_clips
-`);
 
-llamada
-    .then((response) =>{
+document.getElementById('searchButton').addEventListener('click', () => {
+    const term = document.getElementById('searchTerm').value;
+    const llamada = fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${term}&limit=9&offset=0&rating=g&lang=es&bundle=messaging_non_clips`);
+    
+
+    llamada
+        .then((response) =>{
         return response.json();
-})
-   .then((results) => {
+        })
+        .then((results) => {
         //let url = results.data [0].images.original.url;
-        console.log(results);
-        results.data.forEach (element =>{
-            console.log (element.images.original.url);
-            let imagen = document.createElement ("img");
-            imagen.setAttribute ("src", element.images.original.url);
-            document.body.appendChild(imagen);
-
+            const gifsContainer = document.querySelector('.gifs');
+            gifsContainer.innerHTML = ''; // Limpiar gifs anteriores
+            results.data.forEach((element) => {
+                let imagen = document.createElement('img');
+                imagen.setAttribute('src', element.images.original.url);
+                gifsContainer.appendChild(imagen);
+            });
+        })
+        .catch((error) => {
+            console.error(error.message);
         });
-   })
-   .catch((error) => {
-        console.error(error.message);
     });
+    
+
+        
 
         
